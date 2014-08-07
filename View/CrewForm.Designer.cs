d41a91a0cccab6c905cf222a183aa19fa10f23a5
fede.caste <fede.caste@gmail.com>
2014-08-07 13:26:59 +0000

    issue 108

--- View/CrewForm.Designer.cs
+++ View/CrewForm.Designer.cs
@@ -37,6 +37,8 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -45,14 +47,14 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 15);
+            this.label1.Size = new System.Drawing.Size(196, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "IVaPS \"crew\" utilities:";
+            this.label1.Text = "IVaPS \"crew\" utilities hotkeys:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 125);
+            this.label2.Location = new System.Drawing.Point(12, 149);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(154, 13);
             this.label2.TabIndex = 1;
@@ -70,7 +72,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 148);
+            this.label4.Location = new System.Drawing.Point(13, 171);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(176, 13);
             this.label4.TabIndex = 3;
@@ -81,42 +83,62 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(13, 60);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(163, 13);
+            this.label5.Size = new System.Drawing.Size(210, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Read checklist Section: CTRL+1";
+            this.label5.Text = "Select a checklist section to read: CTRL+1";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(13, 170);
+            this.label7.Location = new System.Drawing.Point(13, 126);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(258, 13);
+            this.label7.Size = new System.Drawing.Size(245, 13);
             this.label7.TabIndex = 6;
-            this.label7.Text = "Read next section CTRL + 6 or rapid double CTRL+3";
+            this.label7.Text = "Read next checklist section: rapid double CTRL+3";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(13, 82);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(109, 13);
+            this.label6.Size = new System.Drawing.Size(104, 13);
             this.label6.TabIndex = 7;
-            this.label6.Text = "Show Maps: CTRL+2";
+            this.label6.Text = "Show Map: CTRL+2";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(13, 104);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(265, 13);
+            this.label8.Size = new System.Drawing.Size(310, 13);
             this.label8.TabIndex = 8;
-            this.label8.Text = "Stop/Resume speaking, read current section: CTRL+3";
+            this.label8.Text = "Stop/Resume speaking, read current checklist section: CTRL+3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 193);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(203, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Repeat current checklist section CTRL+6";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 215);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(265, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Read previous checklist section: rapid double CTRL+6";
             // 
             // CrewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(298, 197);
+            this.ClientSize = new System.Drawing.Size(334, 241);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
@@ -145,5 +167,7 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
